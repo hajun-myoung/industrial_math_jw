ab70b240065f8749f8115976b1ebe590caba7196
SKYLER <gkmftor@naver.com>
2026-04-12 23:09:25 +0900

    2022270026 박진우 산업수학 0409 과제

--- Homework/0402_hw.js
+++ Homework/0402_hw.js
@@ -1,47 +1,48 @@
 // (1)
 function generateRandomString(length = 10) {
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters.charAt(randomIndex);
-    }
-    return result;
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
 }
 
 // (2)
 function generateRandomStringMinMax(min = 2, max = 10) {
-    const randomLength = Math.floor(Math.random() * (max - min + 1)) + min;
-    return generateRandomString(randomLength);
+  const randomLength = Math.floor(Math.random() * (max - min + 1)) + min;
+  return generateRandomString(randomLength);
 }
+console.log(randomLength);
 
 // (3)
 function generateEmail() {
-    const domains = ['gmail.com', 'korea.ac.kr', 'naver.com'];
-    const randomDomain = domains[Math.floor(Math.random() * domains.length)];
-    const username = generateRandomStringMinMax(2, 6);
-    return `${username}@${randomDomain}`;
+  const domains = ['gmail.com', 'korea.ac.kr', 'naver.com'];
+  const randomDomain = domains[Math.floor(Math.random() * domains.length)];
+  const username = generateRandomStringMinMax(2, 6);
+  return `${username}@${randomDomain}`;
 }
 console.log(generateEmail());
 
 // (4)
 function applyArray(array, func) {
-    const result = [];
-    array.forEach(item => {
-        result.push(func(item));
-    });
-    return result;
+  const result = [];
+  array.forEach((item) => {
+    result.push(func(item));
+  });
+  return result;
 }
 
 // (5)
 const generateUsers = (n) => {
-    const users = [];
-    for (let i = 0; i < n; i++) {
-        users.push({
-            username: generateRandomStringMinMax(2, 8),
-            email: `${generateRandomStringMinMax(2, 10)}@${['gmail.com', 'naver.com', 'korea.ac.kr'][Math.floor(Math.random() * 3)]}`
-        });
-    }
-    return users;
+  const users = [];
+  for (let i = 0; i < n; i++) {
+    users.push({
+      username: generateRandomStringMinMax(2, 8),
+      email: `${generateRandomStringMinMax(2, 10)}@${['gmail.com', 'naver.com', 'korea.ac.kr'][Math.floor(Math.random() * 3)]}`,
+    });
+  }
+  return users;
 };
 console.log(generateUsers(5));
